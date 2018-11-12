25b61adacd74769bb905e537c903428e33243995
hugo ocampo <hdocampo@gmail.com>
2018-11-12 12:27:58 -0300

    Simple React app - Text Editor

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import TextEditor from './TextEditor';
+import BoldMark from './BoldMark';
+import ItalicMark from './ItalicMark';
+import FormatToolbar from './FormatToolbar'
+
+export { 
+    TextEditor, 
+    BoldMark,
+    ItalicMark,
+    FormatToolbar
+}
